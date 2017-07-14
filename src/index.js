1bff05c3212bc01e0ec2b335eef01e5dbd0baf08
Prashant Bhargava <prashant@artifacia.com>
2017-07-14 19:23:35 +0530

    getting images from api and setting A-frame image state

--- src/index.js
+++ src/index.js
@@ -2,25 +2,44 @@ import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'babel-polyfill';
+import axios from 'axios';
 import {Entity, Scene} from 'aframe-react';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const GET_IMAGES_API = 'API';
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+        images: [],
+        selectedImage : null
+    }
+
+    this.getImages();
+  }
+
+  // Function for fetching 360 images from API
+  getImages() {
+      axios.get(GET_IMAGES_API)
+      .then( (imageList) => {
+            let images = imageList.data;
+
+            this.setState({
+                images: images,
+                selectedImage: images[0].pano
+            });
+      });
   }
 
   render () {
     return (
       <Scene>
-
-        <Entity primitive="a-sky" src="https://www.360p.co.uk/wp-content/uploads/2011/05/360-panoramas-truro-park-3-1200x600.jpg"/>
+        <Entity primitive="a-sky" src={this.state.selectedImage} />
         <Entity primitive="a-camera">
           <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
         </Entity>
-
-
       </Scene>
     );
   }
